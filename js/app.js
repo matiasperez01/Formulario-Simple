66d518104900f7b85ab26cdd56f32f7e7728ae3e
matiasperez <matiasperezequiel@gmail.com>
2024-07-12 22:28:41 -0300

    Se corrigieron las validaciones

--- js/app.js
+++ js/app.js
@@ -14,19 +14,24 @@ function cambiarEstiloSimple(){
 }
 
 
-const nombre = document.getElementById('nombre').value;
-const apellido = document.getElementById('apellido').value;
-const pais = document.getElementById('pais').value;
-const email = document.getElementById('email').value;
+
 function validarCampos(){
-    if(nombre === '' || apellido === '' || pais === '' || email === ''){
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const pais = document.getElementById('pais').value;
+    const email = document.getElementById('email').value;
+
+    if(nombre.length < 1 || nombre.length > 20 || apellido.length < 2 || apellido.length > 20 || pais === '' || email === ''){
         alert("Por favor, complete todos los campos");
+    }else{
+        var formatoEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!formatoEmail.test(email)) {
+            alert('Por favor, ingrese un email válido.');
+        }else{
+            alert("Datos ingresados correctamente!");
+        }    
     }
-    var formatoEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!formatoEmail.test(email)) {
-        alert('Por favor, ingrese un email válido.');
-    }
-    var email = document.getElementById('email').value;
+    
 }
 
 
